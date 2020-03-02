31a3fcf59230e5279c8558fa35a715e9f9bf4452
Mario-Miguel <mariomig5@gmail.com>
2020-03-02 19:08:34 +0100

    Pequeños cambios en App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Login from "./components/Login/Login";
@@ -20,9 +19,6 @@ const App = (props) => {
         >
           Learn React. Developed using Docker.
         </a>
-
-       
-
       </header>
     </div>
   );*/
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,14 +3,16 @@ import {AuthButton, LoggedIn} from '@solid/react'
 import { logIn } from "./Authentication";
 
 function Login(props) {
-  console.log(props);
+  //console.log(props);
   return (
     <div id="container">
       <h1 data-testid="login-header">Login</h1>
       <AuthButton popup="https://solid.github.io/solid-auth-client/dist/popup.html" />
       <LoggedIn>
           <p>Buena!</p>
+          
       </LoggedIn>
+      
     </div>
   );
 }
