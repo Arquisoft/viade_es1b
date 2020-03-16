d1a077a20d4bbd79084ba118b5fa1ae82d29d33b
Xurde Mares Omar <UO245110>
2020-03-16 18:25:30 +0100

    Actualizda la dependencia, del WebId

--- src/containers/Upload/upload.component.js
+++ src/containers/Upload/upload.component.js
@@ -1,12 +1,18 @@
 import React from "react";
-
+import  useWebId  from "@inrupt/solid-react-components";
 
 class UploadComponent extends React.Component {
+
+
+
     constructor(){
         super();
-        //Instanciamos la variable, con let:
-        let files;
+        //Instanciamos la variable, con let
+        let files; //Estos son los ficheros, con file[0] accederíamos al primero, que es nuestro caso.
+        //Esto nos da la información del usuario:
+        const webId = useWebId(); //Si no está logeado da null
     }
+
 
     //Esto es lo que hacemos cuando llamamos al método.
     itemHandler(parameter){
@@ -19,6 +25,7 @@ class UploadComponent extends React.Component {
     return(
         //Tras coger el item funciona el botón:
         <input type="file" onChange={this.itemHandler}/>
+
      );
     }
 }
