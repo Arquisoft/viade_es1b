cf4214f717f130fe5f2b1681fe4b7dc828b0fd7a
marshall63991 <marshall6399@gmail.com>
2020-03-31 16:13:06 +0200

    estructura básica de las listas en la pestaña de amigos

--- src/containers/Friends/friends.component.js
+++ src/containers/Friends/friends.component.js
@@ -1,23 +1,74 @@
 import React from 'react';
 import { LoggedOut, LoggedIn } from '@solid/react';
 import { Redirect } from 'react-router-dom';
-import { Trans, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { MisAmigosDiv, PeticionesDiv, MisPeticionesPendientesDiv, ContainerDiv } from './friends.style';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        height: '90%',
+        backgroundColor: theme.palette.background.paper,
+        position: 'relative',
+        overflow: 'auto',
+    },
+    listSection: {
+        backgroundColor: 'inherit',
+    },
+}));
+
 
 const Friends = props => {
     const { t } = useTranslation();
+    const classes = useStyles();
+    var arrayAmigos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3,]; //deberia llamarse a una funcion que obtenga el listado de amigos 
+    var arrayPeticionesEnviadas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3]; //deberia llamarse a una funcion que obtenga el listado de peticiones enviadas
+    var arrayPeticionesRecibidas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3]; //deberia llamarse a una funcion que obtenga el listado de peticiones recibidas
+    //en la funcion onClick de los botones hay que anañir las funciones relativas a la accion que se quiere realizar
+
     return (
         <div>
             <LoggedIn>
                 <ContainerDiv>
                     <PeticionesDiv>
                         <h3>{t('friends.sentRequest')}</h3>
+                        <List className={classes.root} subheader={<li />}>
+                            {arrayPeticionesEnviadas.map((sectionId) => (
+                                <li key={`section-${sectionId}`} className={classes.listSection}>
+                                    <ListItem key={`prueba`}>
+                                        <ListItemText primary={`prueba`} />
+                                    </ListItem>
+                                </li>
+                            ))}
+                        </List>
                     </PeticionesDiv>
                     <MisAmigosDiv>
                         <h3>{t('friends.myFriends')}</h3>
+                        <List className={classes.root} subheader={<li />}>
+                            {arrayAmigos.map((sectionId) => (
+                                <li key={`section-${sectionId}`} className={classes.listSection}>
+                                    <ListItem key={`prueba`}>
+                                        <button>prueba</button>
+                                    </ListItem>
+                                </li>
+                            ))}
+                        </List>
                     </MisAmigosDiv>
                     <MisPeticionesPendientesDiv>
                         <h3>{t('friends.recievedRequest')}</h3>
+                        <List className={classes.root} subheader={<li />}>
+                            {arrayPeticionesRecibidas.map((sectionId) => (
+                                <li key={`section-${sectionId}`} className={classes.listSection}>
+                                    <ListItem key={`prueba`}>
+                                        <button>prueba</button>
+                                    </ListItem>
+                                </li>
+                            ))}
+                        </List>
                     </MisPeticionesPendientesDiv>
 
                 </ContainerDiv>
