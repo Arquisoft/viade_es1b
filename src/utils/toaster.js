6d5adfa32902e0804bef4199344666e52a49b930
Xurde <uo245110@uniovi.es>
2020-04-14 04:11:34 +0200

    Creo que ya estarían todas las dobles comillas

--- src/utils/toaster.js
+++ src/utils/toaster.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { toast } from 'react-toastify';
-import { Toaster } from '../components/Utils';
+import React from "react";
+import { toast } from "react-toastify";
+import { Toaster } from "../components/Utils";
 
 export const errorToaster = (content: String, title: String = null, link: Object) =>
   toast(<Toaster {...{ content, title, type: 'error', link }} />, {
