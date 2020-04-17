390760aa79b3a776004f31129509f3ec1b377b2e
Jorge <uo265135@uniovi.es>
2020-04-17 11:47:59 +0200

    Bajar rutas siguiendo la especificacion y cualquier carpeta

--- src/containers/Download/bajarRutas.js
+++ src/containers/Download/bajarRutas.js
@@ -1,55 +1,66 @@
 import auth from "solid-auth-client";
 import SolidFileClient from "solid-file-client";
 
-
 class bajarRutas {
 
     constructor() {
 
         this.sfc = new SolidFileClient(auth);
         this.rutas = [];
+        this.session = null;        
+        this.tmpFolder = "";
     }
 
     async bajarRutasDePod(direccion) {
+        let session = await auth.currentSession();
+        var id = `${session.webId}`;
         this.rutas = [];
+        this.tmpFolder = id.replace('/profile/card#me', '/public/temp');
         //Dandole al boton se obtendria los contenidos del fichero   
         if (direccion === "")
             alert("Escriba la carpeta donde almacena las rutas");
         else {
             // Leemos toda la carpeta
             try {
-                let folder = await this.sfc.readFolder(direccion);
-                // Leemos los ficheros
-                const files = folder.files;
-                var rutas = false;
-                // Para cada fichero que sea json (o el formato que vaya a ser), lo muestra
-                files.forEach(file => {
-                    if (file.type === "application/json") {
-                        this.loadJSon(file.url, files);
-                        if (!rutas)
-                            rutas = true;
+            let folder = await this.sfc.readFolder(direccion,null);
+            // Leemos los ficheros
+            const files = folder.files;
+            console.log(files);
+            var rutas = false;
+            // Para cada fichero que sea json (o el formato que vaya a ser), lo muestra
+            files.forEach(async file => {
+                if (file.type === "application/json") {
+                    //console.log(this.tmpFolder+"/"+file.name);
+                    this.sfc.copyFile(file.url,this.tmpFolder+"/"+file.name);
+                        this.loadJSon(this.tmpFolder+"/"+file.name);
+                        rutas = true 
                     }
-                });
-            } catch (error) {
+            });
+                if(rutas) {
+                    alert("Rutas bajandose")
+                    await this.sfc.deleteFolder(this.tmpFolder,null);
+                }
+                else {
+                    alert("No hay rutas");
+                }            
+            } catch(error) {
+                console.log(error);
                 alert("No se ha podido encontrar la carpeta en su POD");
-            }
+            }            
         }
     }
 
     // Metodo auxiliar para obtener el objeto json
-    loadJSon(url, files) {
-        var Httpreq = new XMLHttpRequest(); // Solicitud
-        Httpreq.open("GET", url, false);
-        Httpreq.send(null);
-        var jsonRuta = JSON.parse(Httpreq.responseText);
-        this.rutas.push(jsonRuta);
-        if (this.rutas.length === files.length) {
-            alert("Descarga Finalizada");
+    async loadJSon(url) {   
+            var Httpreq = new XMLHttpRequest(); // Solicitud
+            Httpreq.open("GET", url, false);
+            Httpreq.send(null);
+            console.log(Httpreq.responseText);
+            var jsonRuta = JSON.parse(Httpreq.responseText);
+            console.log(jsonRuta);
+            this.rutas.push(jsonRuta);         
         }
-        if (files.length === 0)
-            alert("No hay rutas disponibles");
-    }
-
+    
 }
 
 export default bajarRutas = new bajarRutas();
