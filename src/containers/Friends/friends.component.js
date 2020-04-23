09c7a6f61c6b7653af488dd5231df98ed02f4a3f
Mario-Miguel <mariomig5@gmail.com>
2020-04-23 18:24:43 +0200

    Friends test added

--- src/containers/Friends/friends.component.js
+++ src/containers/Friends/friends.component.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { LoggedOut, LoggedIn, useWebId } from '@solid/react';
-import { Redirect } from 'react-router-dom';
+import { useWebId } from '@solid/react';
 import { useTranslation } from 'react-i18next';
 import { MisAmigosDiv, ContainerDiv, ListaDiv, InputStyle, AmigosDiv, ButtonStyle } from './friends.style';
 import addFriend from '../../viade/Friends/addFriend';
@@ -11,13 +10,14 @@ const Friends = props => {
     const name = useWebId();
     var friends = [];
     (async () => {
-        friends = await addFriend.friends
-        var str = '<List>'
-        friends.forEach(function (friend) {
-            str += '<li style="list-style-type: none;"><input name="food" type="radio" value=' + friend + ' id = "radio">' + friend + '</li>';
-        });
-        str += '</List>';
         try {
+            friends = await addFriend.friends
+            var str = '<List>'
+            friends.forEach(function (friend) {
+                str += '<li style="list-style-type: none;"><input name="food" type="radio" value=' + friend + ' id = "radio">' + friend + '</li>';
+            });
+            str += '</List>';
+
             document.getElementById("lista").innerHTML = str;
         }
         catch (e) {
@@ -28,46 +28,41 @@ const Friends = props => {
     return (
         <section>
             <NotificationContainer />
-            <LoggedIn>
-                <ContainerDiv>
-                    <ListaDiv>
-                        <h3>{t('friends.myFriends')}</h3>
-                        <AmigosDiv id="lista">
-                        </AmigosDiv>
-                        <ButtonStyle onClick={(event) => addFriend.removeFriend(event, name, t('friends.deleted'), t('friends.choose'))}>
-                            <img src={process.env.PUBLIC_URL + "/img/icon/rubbish.svg"} width="35" height="35" alt="" />
-                            {t('friends.remove')}
-                        </ButtonStyle>
-                        <ButtonStyle>
-                            <img src={process.env.PUBLIC_URL + "/img/icon/share.svg"} width="35" height="35" alt="" />
-                            {t('friends.share')}
-                        </ButtonStyle>
-                    </ListaDiv>
-                    <MisAmigosDiv>
-                        <h3>{t('friends.addFriend')}</h3>
-                        <form className="modal-body">
-                            <InputStyle type="text" placeholder="https://marshall.solid.community/profile/card#me" id="input" />
-                            <button onClick={(event) => addFriend.addFriend(event, document.getElementById('input').value, name, t('friends.added'), t('friends.empty'), t('friends.webIdF'))} className="send">
-                                <span className="icon">
-                                    <img src={process.env.PUBLIC_URL + "/img/icon/arrow.svg"} width="25" height="20" alt="" />
-                                </span>
-                            </button>
-                        </form>
-                    </MisAmigosDiv>
-                    <ListaDiv>
-                        <h3>{t('friends.group')}</h3>
-                        <AmigosDiv id="groups">
-                        </AmigosDiv>
-                        <ButtonStyle>
-                            <img src={process.env.PUBLIC_URL + "/img/icon/network.svg"} width="35" height="35" alt="" />
-                            {t('friends.add')}
-                        </ButtonStyle>
-                    </ListaDiv>
-                </ContainerDiv>
-            </LoggedIn>
-            <LoggedOut>
-                <Redirect to='/login'></Redirect>
-            </LoggedOut>
+            <ContainerDiv>
+                <ListaDiv>
+                    <h3>{t('friends.myFriends')}</h3>
+                    <AmigosDiv id="lista">
+                    </AmigosDiv>
+                    <ButtonStyle data-testid="remove-button" onClick={(event) => addFriend.removeFriend(event, name, t('friends.deleted'), t('friends.choose'))}>
+                        <img src={process.env.PUBLIC_URL + "/img/icon/rubbish.svg"} width="35" height="35" alt="" />
+                        {t('friends.remove')}
+                    </ButtonStyle>
+                    <ButtonStyle data-testid="share-button">
+                        <img src={process.env.PUBLIC_URL + "/img/icon/share.svg"} width="35" height="35" alt="" />
+                        {t('friends.share')}
+                    </ButtonStyle>
+                </ListaDiv>
+                <MisAmigosDiv data-testid="add-friend">
+                    <h3>{t('friends.addFriend')}</h3>
+                    <form className="modal-body">
+                        <InputStyle type="text" placeholder="https://marshall.solid.community/profile/card#me" id="input" />
+                        <button data-testid="add-friend-button" onClick={(event) => addFriend.addFriend(event, document.getElementById('input').value, name, t('friends.added'), t('friends.empty'), t('friends.webIdF'))} className="send">
+                            <span className="icon">
+                                <img src={process.env.PUBLIC_URL + "/img/icon/arrow.svg"} width="25" height="20" alt="" />
+                            </span>
+                        </button>
+                    </form>
+                </MisAmigosDiv>
+                <ListaDiv>
+                    <h3>{t('friends.group')}</h3>
+                    <AmigosDiv id="groups">
+                    </AmigosDiv>
+                    <ButtonStyle data-testid="friend-group-button">
+                        <img src={process.env.PUBLIC_URL + "/img/icon/network.svg"} width="35" height="35" alt="" />
+                        {t('friends.add')}
+                    </ButtonStyle>
+                </ListaDiv>
+            </ContainerDiv>
         </section >
     );
 };
