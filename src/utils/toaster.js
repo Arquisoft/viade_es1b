df1ed580e41310ac61ce61de517ed8cae7c5fb08
Xurde <uo245110@uniovi.es>
2020-04-27 18:51:16 +0200

    Code Quality: Relativo a Upload component

--- src/utils/toaster.js
+++ src/utils/toaster.js
@@ -3,14 +3,14 @@ import { toast } from "react-toastify";
 import { Toaster } from "../components/Utils";
 
 export const errorToaster = (content: String, title: String = null, link: Object) =>
-  toast(<Toaster {...{ content, title, type: 'error', link }} />, {
+  toast(<Toaster {...{ content, title, type: "error", link }} />, {
     autoClose: false,
-    className: 'solid-toaster toaster-error',
-    type: 'error'
+    className: "solid-toaster toaster-error",
+    type: "error"
   });
 
 export const successToaster = (content: String, title: String = null, link: Object) =>
-  toast(<Toaster {...{ content, title, type: 'success', link }} />, {
-    className: 'solid-toaster toaster-success',
-    type: 'success'
+  toast(<Toaster {...{ content, title, type: "success", link }} />, {
+    className: "solid-toaster toaster-success",
+    type: "success"
   });
