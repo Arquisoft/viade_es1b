9ede84cd6d42df9523432afa0266a2293cccb42f
marshall63991 <marshall6399@gmail.com>
2020-04-27 21:58:00 +0200

    lista dinamicas de amigos

--- src/containers/Friends/friends.component.js
+++ src/containers/Friends/friends.component.js
@@ -1,33 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LoggedOut, LoggedIn, useWebId } from '@solid/react';
 import { Redirect } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { ListaDiv, InputStyle, AmigosDiv, ButtonStyle, DivStyle1, ButtonStyle2, H3Style } from './friends.style';
+import { ListaDiv, InputStyle, AmigosDiv, ButtonStyle, DivStyle1, ButtonStyle2, ButtonStyle3, H3Style } from './friends.style';
 import { useNotification } from "@inrupt/solid-react-components";
-import addFriend from '../../viade/Friends/addFriend';
+import AddFriend from '../../viade/Friends/addFriend';
 import { NotificationContainer, NotificationManager } from "react-notifications";
 
 const Friends = props => {
     const { t } = useTranslation();
     const name = useWebId();
-    var friends = [];
+
+    var [amigos, setAmigos] = useState([]);
     const { createNotification, discoverInbox } = useNotification(
         name
     );
-    (async () => {
-        friends = await addFriend.friends
-        var str = '<List>'
-        friends.forEach(function (friend) {
-            str += '<li style="list-style-type: none;"><input name="food" type="radio" value=' + friend + ' id = "radio">' + friend + '</li>';
-        });
-        str += '</List>';
-        try {
-            document.getElementById("lista").innerHTML = str;
-        }
-        catch (e) {
 
-        }
-    })()
 
     const sendNotification = async (title, friendId, summary) => {
         var names = name.split(".");
@@ -51,33 +39,50 @@ const Friends = props => {
     };
 
     function addFriendS() {
-        addFriend.addFriend(document.getElementById('input').value, name, t('friends.added'), t('friends.empty'), t('friends.webIdF')).then(ret => {
+        AddFriend.addFriend(document.getElementById('input').value, name, t('friends.added'), t('friends.empty'), t('friends.webIdF')).then(ret => {
             if (ret === 1)
                 sendNotification(t('notifications.titleAdd'), document.getElementById('input').value, t('notifications.summaryAdd'));
         });
     }
+
+    const refresh = async () => {
+        var friends = [];
+        friends = await AddFriend.getFriends()
+        console.log(friends);
+        friends.forEach(function (friend) {
+            if (amigos.includes(friend.toString()) === false)
+                setAmigos(amigos => [...amigos, friend]);
+        });
+        if (amigos.length > friends.length)
+            setAmigos(friends);
+    }
+
 
     return (
         <DivStyle1>
             <LoggedIn>
                 <ListaDiv>
                     <H3Style>{t('friends.addFriend')}</H3Style>
-                    <NotificationContainer />
-
                     <InputStyle type="text" placeholder="https://marshall.solid.community/profile/card#me" id="input" />
+                    <ButtonStyle3 onClick={refresh} > <img src={process.env.PUBLIC_URL + "/img/icon/refresh.svg"} width="25" height="20" alt="" /></ButtonStyle3>
                     <ButtonStyle2 onClick={addFriendS} className="send">
                         <span className="icon">
                             <img src={process.env.PUBLIC_URL + "/img/icon/arrow.svg"} width="25" height="20" alt="" />
                         </span>
                     </ButtonStyle2>
 
+
                     <H3Style>{t('friends.myFriends')}</H3Style>
-                    <AmigosDiv id="lista">
+                    <AmigosDiv className="lista">
+                        <ul>
+                            {amigos.map(item => <li><input name="food" type="radio" value={item} id="radio" ></input>{item}</li>)}
+                        </ul>
                     </AmigosDiv>
-                    <ButtonStyle onClick={(event) => addFriend.removeFriend(event, name, t('friends.deleted'), t('friends.choose'))}>
+                    <ButtonStyle onClick={(event) => AddFriend.removeFriend(event, name, t('friends.deleted'), t('friends.choose'))}>
                         <img src={process.env.PUBLIC_URL + "/img/icon/rubbish.svg"} width="35" height="35" alt="" />
                         {t('friends.remove')}
                     </ButtonStyle>
+                    <NotificationContainer />
                 </ListaDiv>
                 <ListaDiv>
                     <H3Style>{t('friends.group')}</H3Style>
--- src/containers/Notifications/NotificationsList.js
+++ src/containers/Notifications/NotificationsList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useWebId } from "@solid/react";
 import { useTranslation } from 'react-i18next';
 import { useNotification } from "@inrupt/solid-react-components";
@@ -14,45 +14,52 @@ const NotificationList = docId => {
         fetchNotification,
     } = useNotification(myWebId);
 
-    class NotificationClass {
+    useEffect(() => {
+        class NotificationClass {
 
-        constructor() {
-            this.notificationA = this.handleNotifications()
-        }
-
-        async  handleNotifications() {
-            const notificationArray = [];
-            if (myWebId !== undefined && myWebId !== null) {
-                let inbox = myWebId.replace("/profile/card#me", "/inbox/");
-                const inboxes = [{ path: inbox, inboxName: 'Global Inbox', shape: 'default' }];
-                await fetchNotification(inboxes);
-                if (notification.notifications.length > 0)
-                    for (let i = 0; i < notification.notifications.length; i++)
-                        if (notification.notifications[i].read !== "true")
-                            notificationArray.push(notification.notifications[i].summary);
+            constructor() {
+                this.notificationA = this.handleNotifications()
             }
-            const ret = await Promise.all(notificationArray);
-            return ret;
-        }
-    }
 
-    var notificationR = new NotificationClass();
-
-    (async () => {
-        var notifications = [];
-        notifications = await notificationR.notificationA
-        var str = '<List>'
-        notifications.forEach(function (notification) {
-            str += '<li><a>' + notification + '</a></li>';
-        });
-        str += '</List>';
-        try {
-            document.getElementById("lis").innerHTML = str;
-        }
-        catch (e) {
+            async  handleNotifications() {
+                const notificationArray = [];
+                if (myWebId !== undefined && myWebId !== null) {
+                    let inbox = myWebId.replace("/profile/card#me", "/inbox/");
+                    const inboxes = [{ path: inbox, inboxName: 'Global Inbox', shape: 'default' }];
+                    await fetchNotification(inboxes);
+                    if (notification.notifications.length > 0)
+                        for (let i = 0; i < notification.notifications.length; i++)
+                            if (notification.notifications[i].read !== "true")
+                                notificationArray.push(notification.notifications[i].summary);
+                }
+                const ret = await Promise.all(notificationArray);
+                return ret;
+            }
 
         }
-    })()
+
+
+
+
+
+        (async () => {
+            var notifications = [];
+
+            notifications = await new NotificationClass().notificationA
+            var str = '<List>'
+            notifications.forEach(function (notification) {
+                str += '<li><a>' + notification + '</a></li>';
+            });
+            str += '</List>';
+            try {
+                document.getElementById("lis").innerHTML = str;
+            }
+            catch (e) {
+
+            }
+        })()
+    });
+
 
     return (
         <DivStyle1>
