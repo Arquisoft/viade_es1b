eed1d8e7f054b9c30ee0076b3ce16c90d9653fac
marshall63991 <marshall6399@gmail.com>
2020-05-07 00:45:42 +0200

    mas arreglos

--- src/containers/Notifications/NotificationsList.js
+++ src/containers/Notifications/NotificationsList.js
@@ -5,7 +5,8 @@ import { useNotification } from "@inrupt/solid-react-components";
 import { DivStyle1, DivStyle, H4Style, DivStyle2, H6Style } from "./notifications.style";
 
 var i = 0;
-const NotificationList = (docId) => {
+var notifs = [];
+var NotificationList = (docId) => {
 
   const myWebId = useWebId();
   const { t } = useTranslation();
@@ -24,21 +25,33 @@ const NotificationList = (docId) => {
       }
     }
     var ret = await Promise.all(notificationArray);
-    var str = "";
+    var str = "<List>";
     ret.forEach(function (notification) {
       str += "<li key= " + notification + "><a>" + notification + "</a></li>";
     });
-    str += "";
+    str += "</List>";
     try {
       document.getElementById("lis").innerHTML = str;
     } catch (e) {
       ret = [];
     }
+    notifs = ret;
   }
 
   if (i < 5) {
     handleNotifications();
     i++;
+  } else {
+    var str = "<List>";
+    notifs.forEach(function (notification) {
+      str += "<li key= " + notification + "><a>" + notification + "</a></li>";
+    });
+    str += "</List>";
+    try {
+      document.getElementById("lis").innerHTML = str;
+    } catch (e) {
+      str = [];
+    }
   }
 
   return (
@@ -48,9 +61,7 @@ const NotificationList = (docId) => {
           {t("notifications.information")}
         </H4Style>
         <H6Style>{t("notifications.information2")}</H6Style>
-        <DivStyle2 data-testid="notifications-list">
-          <ul id="lis">
-          </ul>
+        <DivStyle2 id="lis" data-testid="notifications-list">
         </DivStyle2>
       </DivStyle>
     </DivStyle1 >
